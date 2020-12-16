e72d24c7afcd562b68929d8f5950f70c2405b2c7
jandonggun <donggontrannguyen@gmail.com>
2020-12-16 12:40:40 +0700

    add login provider

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+enum AuthStatus {
+  NotAuthenticated,
+  Authenticating,
+  Authenticated,
+  UserNotFound,
+  Error,
+}
+
+class AuthProvider extends ChangeNotifier {
+  User user;
+  AuthStatus status;
+  FirebaseAuth _auth;
+
+  static AuthProvider instance = AuthProvider();
+  AuthProvider() {
+    _auth = FirebaseAuth.instance;
+  }
+
+  void loginUserWithEmailandPassword(String _email, String _password) async {
+    status = AuthStatus.Authenticating;
+    try {
+      UserCredential _result = await _auth.signInWithEmailAndPassword(
+          email: _email, password: _password);
+      user = _result.user;
+      status = AuthStatus.Authenticated;
+      print('Logged in succesfully');
+    } catch (e) {}
+  }
+}
