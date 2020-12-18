d5eef71475993eb2bf79b360502e111f2539385e
jandonggun <donggontrannguyen@gmail.com>
2020-12-18 23:16:43 +0700

    add tabbar home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import "package:chatify/pages/profile_page.dart";
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  _HomePageState() {
+    _tabController = TabController(length: 3, vsync: this, initialIndex: 1);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +25,42 @@ class _HomePageState extends State<HomePage> {
           headline1: TextStyle(fontSize: 16),
         ),
         title: Text('Chatify'),
+        bottom: TabBar(
+          controller: _tabController,
+          unselectedLabelColor: Colors.grey,
+          indicatorColor: Colors.blue,
+          labelColor: Colors.blue,
+          tabs: [
+            Tab(
+              icon: Icon(
+                Icons.people_outline,
+                size: 25,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.chat_bubble_outline,
+                size: 25,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.person_outline,
+                size: 25,
+              ),
+            ),
+          ],
+        ),
       ),
+      body: _tabBarPages(),
     );
+  }
+
+  Widget _tabBarPages() {
+    return TabBarView(controller: _tabController, children: <Widget>[
+      ProfilePage(),
+      ProfilePage(),
+      ProfilePage(),
+    ]);
   }
 }
